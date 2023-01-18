00c57ad23221e64be094ae27f53c246833b6bf49
Dalton Dayhoff <Dalton.Dayhoff@sdl.usu.edu>
2023-01-17 18:13:36 -0700

    updated functions

--- HW1_Optimal Stopping/HW1_Dayhoff_Dalton.py
+++ HW1_Optimal Stopping/HW1_Dayhoff_Dalton.py
@@ -1,30 +1,26 @@
 from typing import List
 import numpy as np
 from matplotlib import pyplot as plt
-import statistics
+import csv
 
-def generate_random_data(number_of_lists: int, points_per_list: int, upper_bound: int ) -> List[np.ndarray]:
-    data = [np.random.randint(upper_bound, size=(points_per_list)) for i in range(number_of_lists)]
-    return data
 
-def look_and_leap(data: np.ndarray, stopping_index: int):
+def _look_and_leap(data: np.ndarray, stopping_index: int):
     max = 0
     for i, (x) in enumerate(data):
-        if (i > stopping_index) and (x > max):
+        if (i > stopping_index) and (x >= max):
             return x
         if x > max:
             max = x
         elif i == (data.size - 1):
             return x
 
-def test_random_data(data_points: int) -> List[float]:
-    data = generate_random_data(1000, data_points, 100)
+def test_data(data: List[np.ndarray]) -> List[float]:
     overall_stats = []
-    for i in range(data[0].size):
+    for i in range(data[0].size): #data[0].size gives the number of data points per list
         individual_stats = []
         print("i = ",i)
         for data_set in data:
-            max_found = look_and_leap(data_set, i)
+            max_found = _look_and_leap(data_set, i)
             max_value = max(data_set)
             
             if max_found == max_value:
@@ -34,10 +30,36 @@ def test_random_data(data_points: int) -> List[float]:
         overall_stats.append(sum(individual_stats)/len(individual_stats))
     return overall_stats
 
+def test_random_data(data_points: int, number_of_lists: int, upper_bound_of_integers: int):
+    data = [np.random.randint(upper_bound_of_integers, size=(data_points)) for i in range(number_of_lists)]
+    return test_data(data)
+
+def read_file(name: str):
+    data = []
+    with open(name, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for val in reader:
+            data.append(int(val[0]))
+    numpy_array = np.array(data)
+    return numpy_array
+
+
 
 if __name__ == "__main__":
-    number_of_data_points = 100
-    data = test_random_data(number_of_data_points)
-    x_values = range(0,number_of_data_points)
-    plt.plot(x_values, data)
+    data_points = 1000
+    lists = 1000
+    upper_bound = 1000
+    # scenario_1 = read_file('scenario1.csv')
+    # scenario_2 = read_file('scenario2.csv')
+    random_results = test_random_data(data_points, lists, upper_bound)
+    # s1_results = test_data([scenario_1])
+    # s2_results = test_data([scenario_2])
+    x_values = [i/10 for i in range(data_points)]
+    plt.plot(x_values, random_results, label = 'Random Set of Lists')
+    # plt.plot(x_values, s1_results)
+    # plt.plot(x_values, s2_results)
+    plt.axvline(x = .37*data_points/10, color = 'k', label = '37 percent')
+    plt.xlabel('Percent of data searched')
+    plt.ylabel('Success rate')
     plt.show()
+
