5d43b47390e3bc577db85971a2f93622de56f187
Dalton-Dayhoff <daltondayhoff55@gmail.com>
2023-01-18 17:57:22 -0700

    updated data handling

--- HW1_Optimal Stopping/HW1_Dayhoff_Dalton.py
+++ HW1_Optimal Stopping/HW1_Dayhoff_Dalton.py
@@ -27,7 +27,7 @@ def test_data(data: List[np.ndarray]) -> List[float]:
                 individual_stats.append(1)
             else:
                 individual_stats.append(0)
-        overall_stats.append(sum(individual_stats)/len(individual_stats))
+        overall_stats.append(sum(individual_stats)/len(individual_stats) * 100)
     return overall_stats
 
 def test_random_data(data_points: int, number_of_lists: int, upper_bound_of_integers: int):
@@ -43,22 +43,26 @@ def read_file(name: str):
     numpy_array = np.array(data)
     return numpy_array
 
-
+def run_scenarios():
+    scenario_1 = read_file('scenario1.csv')
+    scenario_2 = read_file('scenario2.csv')
+    s1_results = test_data([scenario_1])
+    s2_results = test_data([scenario_2])
+    percent_normalizer = scenario_1.size/100
+    x_values = [i/percent_normalizer for i in range(scenario_1.size)]
+    plt.plot(x_values, s1_results)
+    plt.plot(x_values, s2_results)
+    plt.show()
 
 if __name__ == "__main__":
-    data_points = 1000
+    data_points = 100
     lists = 1000
     upper_bound = 1000
-    # scenario_1 = read_file('scenario1.csv')
-    # scenario_2 = read_file('scenario2.csv')
+    percent_normalizer = data_points/100
     random_results = test_random_data(data_points, lists, upper_bound)
-    # s1_results = test_data([scenario_1])
-    # s2_results = test_data([scenario_2])
-    x_values = [i/10 for i in range(data_points)]
+    x_values = [i/percent_normalizer for i in range(data_points)]
     plt.plot(x_values, random_results, label = 'Random Set of Lists')
-    # plt.plot(x_values, s1_results)
-    # plt.plot(x_values, s2_results)
-    plt.axvline(x = .37*data_points/10, color = 'k', label = '37 percent')
+    plt.axvline(x = .37*data_points/percent_normalizer, color = 'k', label = '37 percent')
     plt.xlabel('Percent of data searched')
     plt.ylabel('Success rate')
     plt.show()
